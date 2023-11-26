65862c62631fbe51d18e09469388c9fc62ae58d2
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2023-11-27 01:55:42 +0800

    feat: error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { logger } from './middlewares/log';
 import { usersRoutes } from './routes/users';
+import { errorHandler } from './middlewares/error';
 
 const app = express();
 const PORT = 5000;
@@ -14,6 +15,8 @@ app.use('/users', usersRoutes);
 app.get('/', (_, res) => {
   res.send('Hello Express!');
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
---
+++ src/middlewares/error.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err.stack);
+  res.status(500).send(err.message);
+};
