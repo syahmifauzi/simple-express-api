e43306e57d20a95e0f9db75f5f123fcea4d48698
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2023-12-01 20:05:15 +0800

    feat: implement rate limiter

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import express from 'express';
 import { logger } from './middlewares/log';
 import { usersRoutes } from './routes/users';
 import { errorHandler } from './middlewares/error';
+import { rateLimiter } from './middlewares/limiter';
 
 const app = express();
 
 app.use(logger);
+app.use(rateLimiter);
 
 app.use(express.json());
 app.use('/users', usersRoutes);
---
+++ src/middlewares/limiter.ts
@@ -0,0 +1,8 @@
+import { rateLimit } from 'express-rate-limit';
+
+export const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+});
